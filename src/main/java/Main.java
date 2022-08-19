4b3d566a5cca96f9ab637c9bed72ec46d8c65c9f
EnisMollaahmed <enis_super02@outlook.com>
2022-08-19 17:30:56 +0300

    Оправих четенето от файлове. Трябва да направя рандом числата.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,23 @@
 import java.io.*;
 import java.util.Scanner;
 import java.io.IOException;
-public class Main {
+public class Main {//TODO:ДА ПОСАВЯ ФИГУРИТЕ В МАСИВ, КОЙТО МОЖЕ ДА СИ ПРОМЕНЯ РАЗМЕРА
     public static void main(String[] args){
 
+    }
+    public String extractNumbers(String line){
+        String extractNums = line;
+        extractNums = extractNums.replaceAll("[^\\d]", " ");
+        extractNums = extractNums.trim();
+        extractNums = extractNums.replaceAll(" + ", " ");
+        return extractNums;
+    }
+    public void createRandomFigures(int countOfFigures){//TODO:CREATE FIGURES USING RANDOM NUMBERS
 
     }
     public void readFromFile(String fileName){
+        CreateFigure createFigure = CreateFigure.getInstance();
+        Figure figure;
         try {
             File myFile = new File(fileName);
             if(myFile.createNewFile()){
@@ -18,6 +29,28 @@ public class Main {
             FileReader file = new FileReader(myFile.getAbsolutePath());
             BufferedReader buffReader = new BufferedReader(file);
             String line = buffReader.readLine();
+            String extractFigureName = line;
+            extractFigureName = extractFigureName.replaceAll("[0-9]", "");
+            extractFigureName = extractFigureName.trim();
+            extractFigureName = extractFigureName.toLowerCase();
+            Scanner scanner = new Scanner(extractNumbers(line));
+            switch (extractFigureName){
+                case "circle":
+                    double radius = Double.parseDouble(scanner.next());
+                    figure = new Circle(radius);
+                    break;
+                case"rectangle":
+                    double length = Double.parseDouble(scanner.next());
+                    double width = Double.parseDouble(scanner.next());
+                    figure = new Rectangle(length, width);
+                    break;
+                case "triangle":
+                    double sideA = Double.parseDouble(scanner.next());
+                    double sideB = Double.parseDouble(scanner.next());
+                    double sideC = Double.parseDouble(scanner.next());
+                    figure = new Triangle(sideA, sideB, sideC);
+                    break;
+            }
         }catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
@@ -26,7 +59,7 @@ public class Main {
     public void readFromTheConsole() {
         Scanner scanner = new Scanner(System.in);
         CreateFigure createFigure = CreateFigure.getInstance();
-        Figure figure;//TODO:ДА ПОСТВАЯ ТОВА ВЪВ ФУНКЦИЯ
+        Figure figure;
         String figName;
         do{
             figName = scanner.nextLine();
@@ -34,7 +67,7 @@ public class Main {
         }while(!(figName.equals("circle")) && !(figName.equals("rectangle")) && !(figName.equals("triangle")));
         switch(figName){
             case "circle":
-                System.out.println("Enter circle's radius:");
+                System.out.println("Enter circle's extractRadius:");
                 double rad = Double.parseDouble(scanner.nextLine());
                 figure = createFigure.createCircle(rad);
                 break;
