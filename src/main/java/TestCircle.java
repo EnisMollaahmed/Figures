b3dbef14ff8b0efa2dad9c73ad9df8fc34c6d95f
EnisMollaahmed <enis_super02@outlook.com>
2022-08-21 22:23:28 +0300

    TestCircle.java is added

---
+++ src/main/java/TestCircle.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class TestCircle {
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5, 15})
+    public void testPerimeter(int number){
+        Circle circle1 = new Circle(number);
+        assert(circle1.perimeter() == number * Math.PI * circle1.getRadius());
+    }
+    @Test
+    public void testClone(){
+        Circle circle1 = new Circle(1);
+        Circle circle2 = circle1;
+        assert (circle1.equals(circle2));
+    }
+    @Test
+    public void testToString(){
+        Circle circle = new Circle(5);
+        assert(circle.toString().equals("Circle: radius: 5"));
+    }
+}
